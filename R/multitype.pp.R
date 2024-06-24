33bb0d481eba2e7e0b7f1ce17a50db85316ee82b
Junsouk Choi <jchoi@stat.tamu.edu>
2024-06-24 14:56:07 -0400

    Update the multitype.pp function.

--- R/multitype.pp.R
+++ R/multitype.pp.R
@@ -5,8 +5,6 @@
 #' @param x Vector of \eqn{x} coordinates of data points.
 #' @param y Vector of \eqn{y} coordinates of data points.
 #' @param marks mark values for the points. A vector or data frame.
-#' @param xrange \eqn{x} coordinate limits of enclosing box
-#' @param yrange \eqn{y} coordinate limits of enclosing box
 #' @param poly Optional. Polygonal boundary of window. Incompatible
 #' @param unitname Optional. Name of unit of length. Either a single character string, or a vector of two character strings giving the singular and plural forms, respectively.
 #'
@@ -36,7 +34,8 @@
 #'
 #' # plot the multitype point pattern
 #' plot(pp_lung, main = "Lung cancer image", cols = 2 : 7)
-multitype.pp = function(x, y, marks, xrange = c(min(x), max(x)), yrange = c(min(y), max(y)), poly=NULL, unitname=NULL)
+multitype.pp = function(x, y, marks, poly=NULL, unitname=NULL)
 {
-   return(spatstat.geom::ppp(x = x, y = y, xrange = xrange, yrange = yrange, poly = poly, unitname = unitname, marks = factor(marks)))
+   w = spatstat.geom::convexhull.xy(x, y)
+   return(spatstat.geom::ppp(x = x, y = y, window = w, poly = poly, unitname = unitname, marks = factor(marks)))
 }
